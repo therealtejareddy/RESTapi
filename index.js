af7c454ddc0f681bd2a174a626eb5c43d781c985
root <root@TEJA-REDDY.localdomain>
2022-04-25 23:56:54 +0530

    GET, POST actions added

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Person = require("./models/Person");
+require('dotenv').config()
+
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true
+}, () => {
+    console.log("Connected")
+})
+
+//get all
+app.get("/", async (req,res) => {
+    const data = await Person.find();
+    res.json(data)
+});
+
+//get one
+app.get("/email/:email", async (req,res) => {
+    try{
+        const data = await Person.findOne({email:req.params.email});
+        if(data){
+            res.json(data);
+        }else{
+            res.json({message:"Email not Exist"});
+        }
+    } catch (err) {
+        res.json({message:"Person not Exist"});
+    }
+});
+//post
+app.post("/", async (req,res) => {
+    try{
+        const {name, email} = req.body;
+        await Person.create({name,email});
+        res.json({"message":"Person Created"});
+    }catch(err){
+        res.json({message:"Person Can't create"});
+    }
+    
+})
+
+
+app.listen(3000, console.log("server running..."))
