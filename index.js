863528f95377829e233a0f7585c7ddc001f8b2fb
root <root@TEJA-REDDY.localdomain>
2022-04-26 00:01:40 +0530

    CORS enabled

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Person = require("./models/Person");
 require('dotenv').config()
+var cors = require('cors')
+var app = express()
 
-const app = express()
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
